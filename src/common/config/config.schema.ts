8414413f3e3832ece9351a7c751ebce7b779eac1
KotikVacia <vladatkishkin@vk.com>
2022-10-16 21:43:10 +0500

    module3-task1: Add pino logger + add inversify

---
+++ src/common/config/config.schema.ts
@@ -0,0 +1,31 @@
+import convict from 'convict';
+import validator from 'convict-format-with-validator';
+
+convict.addFormats(validator);
+
+export type ConfigSchema = {
+  PORT: number;
+  DB_HOST: string;
+  SALT: string;
+}
+
+export const configSchema = convict<ConfigSchema>({
+  PORT: {
+    doc: 'Порт для исходящих запросов',
+    format: 'port',
+    env: 'PORT',
+    default: 8000
+  },
+  DB_HOST: {
+    doc: 'IP адрес базы данных MongoDB',
+    format: 'ipaddress',
+    env: 'DB_HOST',
+    default: '127.0.0.1'
+  },
+  SALT: {
+    doc: 'Salt for password hash',
+    format: String,
+    env: 'SALT',
+    default: null
+  }
+});
---
+++ src/common/logger/logger.interface.ts
@@ -0,0 +1,9 @@
+export interface LoggerInterface {
+  info(message: string, ...args: unknown[]): void;
+
+  warn(message: string, ...args: unknown[]): void;
+
+  error(message: string, ...args: unknown[]): void;
+
+  debug(message: string, ...args: unknown[]): void;
+}
